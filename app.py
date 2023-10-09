2df6c729c6ed70a9e701a7acb57c88dc8dbcf173
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-10-09 06:20:09 -0700

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('base.html')
 
 
 @app.route('/services')
