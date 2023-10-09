da03345636bbfc643387d3e333e52fda3d6e051b
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-10-08 23:19:47 -0700

    update

--- app.py
+++ app.py
@@ -6,8 +6,21 @@ app = Flask("scissorsBeautySalon")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
-if "scissorsBeautySalon" == '__main__':
+@app.route('/')
+def Services():
+    if request.method == 'GET':
+        return render_template('services.html')
+
+
+@app.route('/')
+def About():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
